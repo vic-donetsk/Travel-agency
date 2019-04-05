f0c756efb8dc9ca0eecda9fdb8a29c240d471acd
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-04-05 22:57:21 +0300

    some bugs fixed

--- resources/views/components/mob-main-menu.blade.php
+++ resources/views/components/mob-main-menu.blade.php
@@ -13,7 +13,7 @@
                 </a>
             </div>
 
-            <div class="single-btn mob-green mob-240"><i class="fas fa-plus"></i>&nbsp; Добавить тур</div>
+            <div class="single-btn mod-green mod-240"><i class="fas fa-plus"></i>&nbsp; Добавить тур</div>
         </div>
     </div>
 </div>
