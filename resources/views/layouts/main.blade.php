103cb67c6f0f73a1e081015408cb500d729b0a17
Alex <livalex11@gmail.com>
2019-04-11 14:37:12 +0300

    @layout -add csrf

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Турагентство</title>
 
@@ -18,7 +19,7 @@
 
 
   <script src="{{ mix('js/app.js') }}"></script>
-  
+
 </body>
 </html>
 
