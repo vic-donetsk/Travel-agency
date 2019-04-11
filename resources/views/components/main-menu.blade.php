9023702f4e91013fad125d7c39c6f35d7af22dab
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-04-12 00:45:16 +0300

    BEM main menu

--- resources/views/components/main-menu.blade.php
+++ resources/views/components/main-menu.blade.php
@@ -1,32 +1,34 @@
-<nav class="main-menu">
-	<div class="menu-block">
-		<div class="trip-transport">
-			<div class="trip-transport-item"> <a href=""> Автобусные туры </a> </div>
-			<div class="trip-transport-item"> <a href=""> Авиационные туры </a></div>
-			<div class="trip-transport-item"> <a href=""> Круизы </a> </div>
-		</div>
-		@include('components.logo')
-		<div class="right-menu">
-			<div class="user trip-user-item">
-				
-					@include('components.user-or-guest', ['text' => 'Вход/Регистрация'])
-				
+<section class="mainMenu-wrapper">
+	<nav class="mainMenu">
+		<div class="mainMenu_menuBlock">
+			<div class="mainMenu_menuBlock_tripTransport">
+				<div class="tripTransport_item"> <a href=""> Автобусные туры </a> </div>
+				<div class="tripTransport_item"> <a href=""> Авиационные туры </a></div>
+				<div class="tripTransport_item"> <a href=""> Круизы </a> </div>
 			</div>
-			<div>	
-				<a href="#">				
-					<i class="fas fa-shopping-cart"></i> &nbsp; Корзина (3555 <i class="fab fa-btc"> )</i>
-				</a>
+			@include('components.logo')
+			<div class="mainMenu_menuBlock_rightMenu">
+				<div class="user mod_tripUserItem">
+					
+						@include('components.user-or-guest', ['text' => 'Вход/Регистрация'])
+					
+				</div>
+				<div>	
+					<a href="#">				
+						<i class="fas fa-shopping-cart"></i> &nbsp; Корзина (3555 <i class="fab fa-btc"> )</i>
+					</a>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="operation-block">
-		<div class="search-field">
-			<input class="search-tour mod_text-2" type="text" placeholder="Поиск предложения">
-			<a class="search" href="#"><i class="fas fa-search"></i></a>
+		<div class="mainMenu_operationBlock">
+			<div class="mainMenu_operationBlock_searchField">
+				<input class="search-tour mod_text-2" type="text" placeholder="Поиск предложения">
+				<a class="search-sign" href="#"><i class="fas fa-search"></i></a>
+			</div>
+			<div class="single-btn mod-white mod-180">
+				<i class="fas fa-plus"></i>&nbsp; Добавить тур
+			</div>
 		</div>
-		<div class="single-btn mod-white mod-180">
-			<i class="fas fa-plus"></i>&nbsp; Добавить тур
-		</div>
-	</div>
-</nav>
+	</nav>
+</section>
