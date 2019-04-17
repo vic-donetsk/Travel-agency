2b2603d6d38c8ab139b657959c8612446184ed3d
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-04-17 22:33:56 +0300

    trip-block_title rebuild

--- resources/views/main/offers-block.blade.php
+++ resources/views/main/offers-block.blade.php
@@ -1,28 +1,28 @@
 <section class="offers-block">
 	<?php $show_class = ['main-propositions', 'main-propositions', 'next-propositions', 'next-propositions', 'hidden-on-mobile-propositions', 'hidden-on-mobile-propositions','hidden-on-mobile-propositions','hidden-on-mobile-propositions']; ?>
 		
-		<div class="tripBlock">
-			<div class="tripBlock_title">
-				<div class="tripBlock_title_header"> ColorLife рекомендует</div>
-				<div class="tripBlock_title_link">
-					<a href="#">Показать все (598) &nbsp;<i class="fas fa-long-arrow-alt-right"></i></a>
-				</div>
+		<div class="trip-block">
+			<div class="trip-block_title">
+				<p class="trip-block_title_header"> ColorLife рекомендует</p>
+				<!-- <div class="trip-block_title_link"> -->
+					<a class="trip-block_title_link"href="#">Показать все (598) &nbsp;<i class="fas fa-long-arrow-alt-right"></i></a>
+				<!-- </div> -->
 			</div>
-			<div class="tripBlock_cards">
+			<div class="trip-block_cards">
 		       
 	            @each('components.trip-card', $show_class, 'show')
 		       
 			</div>
 		</div>
 
-		<div class="tripBlock">
-			<div class="tripBlock_title">
-				<div class="tripBlock_title_header"> Горящие туры </div>
-				<div class="tripBlock_title_link">
+		<div class="trip-block">
+			<div class="trip-block_title">
+				<div class="trip-block_title_header"> Горящие туры </div>
+				<div class="trip-block_title_link">
 					<a href="#">Показать все (598) &nbsp;<i class="fas fa-long-arrow-alt-right"></i></a>
 				</div>
 			</div>
-			<div class="tripBlock_cards">
+			<div class="trip-block_cards">
 
 	            @each('components.trip-card', $show_class, 'show')
 
