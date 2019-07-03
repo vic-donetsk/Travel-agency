c353680ea0321ec8b46776c1bce9ea56a5f1a4fd
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-07-03 08:24:48 +0300

    -add Nova installed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->nullable();     // поле нужно для Новы
             $table->string('first_name')->nullable();             // имя пользователя
             $table->string('last_name')->nullable();              // фамилия
             $table->string('phone')->nullable();                  // телефон
             $table->string('email')->unique();                    // адрес электронной почты (он же логин)
-            $table->string('avatar');                             // адрес аватарки
+            $table->string('avatar')->nullable();                             // адрес аватарки
             //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');                           // хэш пароля
             //$table->rememberToken();
